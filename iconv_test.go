e58e38dd29978b42f1d4a834852541975eb424eb
Thierry Fournier <tfournier@arpalert.org>
2023-07-14 20:49:50 +0200

    MINOR: Improve tests

--- iconv_test.go
+++ iconv_test.go
@@ -5,10 +5,20 @@ import "testing"
 func Test(t *testing.T) {
 	var err error
 	var out string
+	var expect_01 string = "hello, éé, hello éé"
 
-	out, err = Iconv("utf-8", "iso8859-1", "\xe9\xe9\xe9\xe9\xe9\xe9\xe9\xe9\xe9\xe9\xe9\xe9\xe9")
+	/* test sio => utf8 conversion */
+	out, err = Iconv("utf-8", "iso8859-1", "hello, \xe9\xe9, hello \xe9\xe9")
 	if err != nil {
-		println(err.Error())
+		t.Errorf("%s", err.Error())
 	}
-	println(out)
+	if out != expect_01 {
+		t.Errorf("Expect %q, got %q", expect_01, out)
+	}
+
+	/* Test wrong utf8 sequence, expect error */
+	_, err = Iconv("iso8859-1", "utf-8", string([]byte{0b11100010, 0b10100011, 0b10100011}))
+	if err == nil {
+		t.Errorf("Expect an error, got success")
+	}
 }
